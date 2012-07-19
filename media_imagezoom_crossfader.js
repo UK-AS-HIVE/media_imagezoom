ee10ca4089619d22132dce7bd9dfae90d4204fad
Noah Adler <noah.adler@uky.edu>
2012-07-18 21:11:55 -0400

    Serve layers directly, rather than enumerating all media images as available to the OpenLayers system, which caused performance problems with many images

---
+++ media_imagezoom_crossfader.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function()
+{
+  var map = Drupal.settings.openlayers.maps[jQuery('.openlayers-map').attr('id')];
+  jQuery('.openlayers-map .olControlPanZoomBar').prepend('<div id="lichfieldLayerFader" style="position: relative; left: 50px;" class="clearfix"></div>');
+  var baseSelect = '<select class="baseLayerSelect">';
+  var opacitySlider = '<div id="opacitySlider" style="display: inline-block; width: 160px; font-size: 10px; margin: 5px;"></div>';
+  var overlaySelect = '<select class="overlayLayerSelect">';
+  //for (var i=0; i<map.layers.length; ++i)
+  var i=0;
+  for (layername in map.layers)
+  {
+    if (map.layers[layername].isBaseLayer)
+      baseSelect += '<option value="' + i + '">' + map.layers[layername].title + '</option>';
+    else
+      overlaySelect += '<option value="' + i + '">' + map.layers[layername].title + '</option>';
+    i++;
+  }
+  baseSelect += '</select>';
+  overlaySelect += '</select>';
+  jQuery('#lichfieldLayerFader').prepend(overlaySelect);
+  jQuery('#lichfieldLayerFader').prepend(opacitySlider);
+  jQuery('#lichfieldLayerFader').prepend(baseSelect);
+
+  jQuery('#opacitySlider').slider({slide: function(event,ui) {setLayerOpacity(ui.value/100.0);} });
+
+  jQuery('select.baseLayerSelect').change(function() {
+    var ol = jQuery('#' + jQuery('.openlayers-map').attr('id')).data('openlayers').openlayers;
+    ol.setBaseLayer(ol.layers[jQuery(this).val()]);
+  });
+  jQuery('select.overlayLayerSelect').change(function() {
+    var openLayersId = jQuery('.openlayers-map').attr('id');
+    var mapLayers = jQuery('#' + openLayersId).data('openlayers').openlayers.layers;
+    var selectedLayerIndex = jQuery(this).val();
+    for (i in mapLayers)
+    {
+      if (!mapLayers[i].isBaseLayer)
+        mapLayers[i].setVisibility(i == selectedLayerIndex);
+    }
+  });
+});
+
+function setLayerOpacity(opacity)
+{
+  //var mapLayers = Drupal.settings.openlayers.maps[jQuery('.openlayers-map').attr('id')];
+  var mapLayers = jQuery('#' + jQuery('.openlayers-map').attr('id')).data('openlayers').openlayers.layers;
+  for (i in mapLayers)
+  {
+    if (!mapLayers[i].isBaseLayer)
+      mapLayers[i].setOpacity(opacity);
+  }
+}
+
